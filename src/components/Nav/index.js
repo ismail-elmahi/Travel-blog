e2c31ff91630922d7f48af3d1f51a73502a9786f
ismail el mahi <ismail.elmahi1@gmail.com>
2020-02-15 12:43:39 +0100

    update the nav link

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -10,7 +10,7 @@ const Nav = () => {
             <div className="nav__items">
                 <a className="nav__item--left" href="/"><img src={logo} className="nav__item--logo" alt="traveler Pack Logo"/></a>
                 <Link to="/contact" className={window.location.href.indexOf('contact') > 0 ? 'nav__item--link active': 'nav__item--link'}>Contact</Link>
-                <Link to="/blog/all" className={window.location.href.indexOf('blog') > 0 || window.location.href.indexOf('category') > 0 ? 'nav__item--link active': 'nav__item--link'}>Blog</Link>
+                <Link to="/blog/all" className={window.location.href.indexOf('blog/all') > 0 || window.location.href.indexOf('category') > 0 ? 'nav__item--link active': 'nav__item--link'}>Blog</Link>
             
             </div>
         </nav>
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,19 @@
 import React from "react"
-
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import { navigate } from "gatsby"
+import "./contact.css"
+const NotFond = () => {
+  return (
+    <Layout>
+      <div className="contact__header"></div>
+      <div className="contact__thanks">
+        <h1>This Page Not Found .</h1>
+        <button className="btn__med" onClick={() => navigate("/")}>
+          Back to home
+        </button>
+      </div>
+    </Layout>
+  )
+}
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
-
-export default NotFoundPage
+export default NotFond
